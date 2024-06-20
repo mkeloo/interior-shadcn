157c703a686ee036aec3b6c2e34da587fbebaebb
mkeloo <mkeloo@ufl.edu>
2024-06-20 07:52:58 -0400

    project setup with theme toggler.

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import ThemeToggler from './ThemeToggler';
+
+const Header = () => {
+  return (
+    <div>
+      {/* Theme Toggler */}
+      <ThemeToggler />
+    </div>
+  );
+};
+export default Header;
