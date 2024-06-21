77c815aa7e1be3703809132091496791b1efb9d4
mkeloo <mkeloo@ufl.edu>
2024-06-21 02:12:27 -0400

    done with about section. onto Catalog section.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import HeroSection from '@/components/HeroSection';
 import CompanySection from '@/components/CompanySection';
 import AboutSection from '@/components/AboutSection';
+import CatalogSection from '@/components/CatalogSection';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <CompanySection />
       <AboutSection />
+      <CatalogSection />
     </>
   );
 }
---
+++ components/CatalogSection.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const CatalogSection = () => {
+  return <div>CatalogSection</div>;
+};
+export default CatalogSection;
