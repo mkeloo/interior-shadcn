a17c3e08537ccc2e5b256aaea90ed533e8846b15
mkeloo <mkeloo@ufl.edu>
2024-06-21 00:36:05 -0400

    done with Hero. Company section.

---
+++ components/CompanySection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CompanySection = () => {
+  return (
+    <div>
+      <div>
+        <dl>{/* Badge 1 */}</dl>
+      </div>
+    </div>
+  );
+};
+export default CompanySection;
