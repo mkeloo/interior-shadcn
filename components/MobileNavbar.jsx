395aacf5ab555eacdabd0ee255998cf69c3c8aff
mkeloo <mkeloo@ufl.edu>
2024-06-20 23:44:11 -0400

    Finished w/ mobile nav. Onto Hero section.

--- components/MobileNavbar.jsx
+++ components/MobileNavbar.jsx
@@ -1,4 +1,27 @@
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import Navbar from './Navbar';
+import Logo from './Logo';
+import { AlignJustify } from 'lucide-react';
+
 const MobileNavbar = () => {
-  return <div>MobileNavbar</div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+
+      <SheetContent>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Navbar
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-1xl font-semibold"
+            />
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 export default MobileNavbar;
