48db03b8436641f228fd0c37e30fd03cddc9a79c
mkeloo <mkeloo@ufl.edu>
2024-06-21 01:59:47 -0400

    done with company stats section. onto about section.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import HeroSection from '@/components/HeroSection';
+import CompanySection from '@/components/CompanySection';
+import AboutSection from '@/components/AboutSection';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <CompanySection />
+      <AboutSection />
     </>
   );
 }
